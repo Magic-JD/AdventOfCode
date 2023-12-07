4b8a9b20b70f58969a2f4c01b3af7561ec658460
MagicJoe <mag1ctr1cks@hotmail.com>
2023-12-07 13:05:51 +0700

    uodate for 6 and 7

--- src/tasks/Six.java
+++ src/tasks/Six.java
@@ -1,16 +1,16 @@
+package tasks;
 
 import tools.InternetParser;
 import tools.LineUtils;
 
-import java.util.*;
-import java.util.stream.Stream;
+import java.util.Arrays;
+import java.util.List;
 
-import static tools.LineUtils.split;
-
-public class Main {
+public class Six {
 
     public static final String testData = """         
-            
+            Time:      7  15   30
+            Distance:  9  40  200             
                      """;
 
 
@@ -19,13 +19,26 @@ public class Main {
      */
     public static void main(String[] args) {
         List<String> testInput = Arrays.stream(testData.split("\n")).toList();
-        List<String> mainInput = InternetParser.getInput(8);
-        run(testInput, "-", System.currentTimeMillis());
+        List<String> mainInput = InternetParser.getInput(6);
+        run(testInput, "71503", System.currentTimeMillis());
         run(mainInput, "???", System.currentTimeMillis());
     }
 
     public static void run(List<String> input, String expectedOutput, long startTime) {
-        String answer = "";
+        long time = LineUtils.extractLong(input.get(0).split(":")[1]);
+        long distance = LineUtils.extractLong(input.get(1).split(":")[1]);
+        int sum = 1;
+        int noBeating = 0;
+
+        for (int j = 1; j < time; j++) {
+            long total = j * (time - j);
+            if (total > distance) {
+                noBeating++;
+            }
+
+
+        }
+        String answer = "" + noBeating;
         showAnswer(answer, expectedOutput, startTime);
     }
 
